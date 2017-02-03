664d22408b383729745a3ab071fceb44145ae39b
Skalsky Martin <martin.skalsky2@honeywell.com>
2017-02-04 00:48:08 +0100

    hook new test for review and verifies that ff is possible

--- hook.sh
+++ hook.sh
@@ -2,7 +2,15 @@ if grep -q "\review" "$1"; then
   echo preparing review...;
   echo getting latest patch ...;
   
-  
+    common="$(git merge-base HEAD master)" ;
+    master_head="$(git rev-parse master)" ;
+    
+    if [ $common == $master_head ] ; then 
+      echo FF tested...;
+    
+    else 
+      echo FF failed...;
+    fi
  
 fi
 
