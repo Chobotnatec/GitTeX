9612b32ec309de2775ff205cec78da018f460f18
Skalsky Martin <martin.skalsky2@honeywell.com>
2017-02-05 20:52:52 +0100

    some changes in the hooks

--- hook.sh
+++ hook.sh
@@ -1,16 +1,39 @@
 if grep -q "\review" "$1"; then 
-  echo preparing review...;
-  echo getting latest patch ...;
+  echo Preparing review...;
+
+
+  
+  echo Testing Fast-Forward possibility for master HEAD...;
   
     common="$(git merge-base HEAD master)" ;
     master_head="$(git rev-parse master)" ;
     
-    if [ $common == $master_head ] ; then 
-      echo FF tested...;
-    
+    if [ $common == $master_head ]; then 
+      echo Fast-Forward possible...; 
     else 
-      echo FF failed...;
+      echo Fast-Forward impossible...;
+      return 1; 
     fi
- 
+    
+    #send email to each on list
+    
+    #create new branch 
+
+    git branch HEAD "patch_$()
+    
+    #send email
+
 fi
+
+
+if grep -q "\approved" "$1"; then 
+  echo Review has been approved...;
+  
+  common="$(git merge-base HEAD master)"  ;
+  master_head "$(git rev-parse master"    ;
+  git merge master $(current_branch) --no-ff 
+  
+
+  
+  
 
